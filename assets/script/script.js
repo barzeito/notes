254ba26e524e0f694c0577b9eba4c1099e86747c
Bar Zeitony <138291269+barzeito@users.noreply.github.com>
2023-09-06 21:52:11 +0300

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,10 @@
+var openPopup = () => {
+  const popupBox = document.querySelector(".popup-box");
+  popupBox.classList.add("show");
+};
+
+var closePopup = () => {
+    const popupBox = document.querySelector(".popup-box");
+    popupBox.classList.remove("show");
+  };
+
